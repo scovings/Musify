0faf7111fba960a06667e121bf68f238fa3395d9
sco. <77859647+scovings@users.noreply.github.com>
2025-06-07 15:13:30 +0100

    initial-setup

---
+++ app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+
+/**
+ * node modules
+ */
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
+
+// Initial express app
+const express = require('express');
+const app = express();
+
+
+app.listen(5000, () => {
+    console.log
+})
